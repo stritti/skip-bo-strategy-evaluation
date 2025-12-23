63e41fc6592e54957dc048b35c23dc42a2cf789d
Strittmatter, Stephan <Stephan.Strittmatter@sybit.de>
2025-12-23 14:32:37 +0100

    fix: corrected for Tailwind 4 without PostCSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        tailwindcss(),
+        vue()
+    ],
     base: '/', // Root path for Netlify deployment
 })
